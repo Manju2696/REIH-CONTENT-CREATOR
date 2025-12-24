80eb521824a45c093c45105f67eeaa03c753e65c
manju2696 <manjunath.bc@styldod.com>
2025-12-24 13:11:19 +0530

    Update TikTok verification code

--- pages/tiktok_verification.py
+++ pages/tiktok_verification.py
@@ -8,4 +8,4 @@ import streamlit as st
 def show():
     """Display TikTok verification code"""
     st.title("TikTok Verification")
-    st.text("tiktok-developers-site-verification=Cy4tDcp3iWu388XGtTIUGXRzUlVYR7Se")
+    st.text("tiktok-developers-site-verification=p79TO0fCAz5wuEDkZUERrdgwGfAQX0wI")
